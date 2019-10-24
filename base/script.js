5fce4afae4b5659e147f8341422f31ac7eccab15
Denis-Str <web_life@inbox.ru>
2019-10-24 18:39:13 +0300

    add dropdown menu

--- base/script.js
+++
@@ -1,7 +0,0 @@
-let timetable = document.querySelector('.contacts__timetable_active');
-let mode = document.querySelector('.contacts__mode');
-
-timetable.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  mode.classList.toggle('contacts__mode_hidden')
-});
