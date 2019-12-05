1d546d85de29ba86751b164300a32c39576d3ecd
Denis-Str <web_life@inbox.ru>
2019-12-05 19:10:22 +0300

    сделал раскритие элемента по клику в секции команда

--- beginners/script.js
+++ beginners/script.js
@@ -1,14 +1,32 @@
 const btn = document.querySelector('.close-btn');
-const navMenu = document.querySelector('.nav_mobile');
-const arrowLeft = document.querySelector('.slider__arrow-left');
-const arrowRight = document.querySelector('.slider__arrow-right');
-const sliderList = document.querySelector('.slider__list');
 
+// закрытие нав меню
+const navMenu = document.querySelector('.nav_mobile');
 btn.addEventListener('click', (evt) => {
     evt.preventDefault();
     btn.classList.toggle('close-btn_active');
     navMenu.classList.toggle('nav_mobile-show');
 });
+
+//переключение в секции команда
+const accordItems = document.querySelectorAll('.accordion__item');
+
+accordItems.forEach((item, index, list) => {
+    item.addEventListener('click', (event) => {
+        list.forEach(classItem => {
+            classItem.classList.remove('accordion__item_active');
+        });
+        item.classList.add('accordion__item_active');
+        item.addEventListener('click', ()=> {
+            item.classList.toggle('accordion__item_active');
+        })
+    });
+});
+
+// слайдер
+const arrowLeft = document.querySelector('.slider__arrow-left');
+const arrowRight = document.querySelector('.slider__arrow-right');
+const sliderList = document.querySelector('.slider__list');
 
 arrowLeft.addEventListener('click', (e) => {
     e.preventDefault();
@@ -20,5 +38,3 @@ arrowRight.addEventListener('click', (e) => {
 });
 
 
-console.log(arrowLeft);
-console.log(sliderList);
