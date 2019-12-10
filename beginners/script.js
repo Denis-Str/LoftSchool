71feb1e4de2f88c7797d47495a70347fa0b9249d
Denis-Str <web_life@inbox.ru>
2019-12-10 19:54:04 +0300

    обработка и валидация формы

--- beginners/script.js
+++ beginners/script.js
@@ -8,55 +8,151 @@ btn.addEventListener('click', (evt) => {
     navMenu.classList.toggle('nav_mobile-show');
 });
 
-//переключение в секции команда
+//переключение аккордиона
 const accordItems = document.querySelectorAll('.accordion__item');
+const menuItems = document.querySelectorAll('.menu__item');
 
-accordItems.forEach(item => {
-    item.addEventListener('click', () => {
-        if (item.classList.contains('accordion__item_active')) {
-            item.classList.remove('accordion__item_active');
-        } else {
-            accordItems.forEach(item => {
-                item.classList.remove('accordion__item_active');
-            });
-            item.classList.toggle('accordion__item_active');
-        }
-    })
-});
+const accordToggle = (arrList, activeClass) => {
+    arrList.forEach(item => {
+        item.addEventListener('click', () => {
+            if (item.classList.contains(activeClass)) {
+                item.classList.remove(activeClass);
+            } else {
+                arrList.forEach(item => {
+                    item.classList.remove(activeClass);
+                });
+                item.classList.toggle(activeClass);
+            }
+        })
+    });
+};
+
+accordToggle(accordItems, 'accordion__item_active');
+accordToggle(menuItems, 'menu__item_active');
+
 
 // слайдер
 const arrowLeft = document.querySelector('.slider__arrow-left');
 const arrowRight = document.querySelector('.slider__arrow-right');
 const sliderList = document.querySelector('.slider__list');
-let current = 0;
+const sliderEnd = sliderList.childElementCount - 1;
+
+let current = sliderList.offsetWidth;
 let count = 0;
+
+
+const sliderMove = (number) => {sliderList.style.transform = `translateX(${number}px)`};
 
 arrowLeft.addEventListener('click', (e) => {
     e.preventDefault();
-    current = sliderList.offsetWidth;
 
     if (count < 0) {
         count += current;
-        sliderList.style.transform = `translateX(${count}px)`;
+        sliderMove(count);
     }
     else {
         count = -current * 2;
-        sliderList.style.transform = `translateX(${-current * 2}px)`;
+        sliderMove(count);
     }
 });
+
 arrowRight.addEventListener('click', (e) => {
     e.preventDefault();
-    current = -sliderList.offsetWidth;
-
-    if (count >  current * (sliderList.childElementCount - 1)) {
-        count += current;
-        sliderList.style.transform = `translateX(${count}px)`;
+    if (count >  -current * sliderEnd) {
+        count -= current;
+        sliderMove(count);
     }
     else {
-        // count = current * (sliderList.childElementCount - 1);
         count = 0;
-        sliderList.style.transform = 'translateX(0px)';
+        sliderMove(count);
     }
+});
+
+
+
+// форма
+
+const form = document.querySelector('.form');
+const sendBtn = document.querySelector('.form__send');
+const modal = document.querySelector('.send-modal');
+const modalClose = document.querySelector('.send-modal_close');
+// const checkboxValue = value => value ? console.log('Не перезванивать') : console.log('Перезванивать');
+
+// перебор элементов формы
+const formElements = (form) => {
+    let arr = [];
+    for (let i = 0; i < form.length; i++) {
+        if (form[i].type !== 'submit' && form[i].type !== 'reset')
+        arr.push(form[i]);
+    }
+    return arr;
+};
+
+//Проверка полей формы
+const formValidate = (form) => {
+  let valid = true;
+    formElements(form).forEach(elem => {
+        if (!fieldValidate(elem)) {
+            valid = false;
+        }
+    });
+    console.log('formValidate ' + valid);
+    return valid;
+};
+
+//Проверка поля
+const fieldValidate = (elem) => {
+    if (elem.type === 'text' || elem.type === 'textarea') {
+        elem.nextElementSibling.textContent = elem.validationMessage;
+    }
+    return elem.checkValidity();
+};
+
+// получение значений полей из формы
+const formDate = (form) => {
+    let date = {};
+    formElements(form).forEach(item => {
+        let nameElem = item.name;
+        if (item.type === 'text' || item.type === 'textarea') {
+            date[nameElem] = item.value;
+        }
+        if (item.type === 'checkbox') {
+            item.checked ? date.call = ('Не перезванивать') : date.call = 'Перезванивать';
+        }
+    });
+    (form.elements.payment.value === 'cash') ?
+        date.payment = 'Потребуется сдача' :
+        date.payment =  'Оплата по карте';
+    date.to = 'test@test.ru';
+    return date;
+};
+
+// отправка формы
+sendBtn.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    if (formValidate(form)) {
+        modal.classList.add('send-modal_show');
+    }
+    if (formValidate(form)) {
+        const data = formDate(form);
+
+        const xhr = new XMLHttpRequest();
+        xhr.responseType = 'json';
+        xhr.open('POST', 'https://webdev-api.loftschool.com/sendmail');
+        xhr.send(JSON.stringify(data));
+        xhr.addEventListener('load', ()=> {
+            if (console.log(xhr.response.status === true)) {
+                console.log(xhr.response.status);
+            }
+        });
+    }
+});
+
+// закрытие модольного окна формы
+
+modalClose.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    modal.classList.remove('send-modal_show');
 });
 
 // карта
